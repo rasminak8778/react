6dda386b546dfc7362f0c781c91de7c5cba45ac2
rasminak8778 <demodock7@gmail.com>
2024-03-29 17:57:39 +0530

    [ADD]my app

---
+++ my-app/src/component/demo.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  console.log('hey')
+  return (
+    <div className="App">
+      <header>
+         hello 
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+  
